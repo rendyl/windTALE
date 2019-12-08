b61ddee18cf2475347798e3dc428bdd8e3224a4c
p1516923 <rendy.latbi@etu.univ-lyon1.fr>
2019-12-09 00:53:38 +0100

    init

---
+++ windTALE/Assets/SpawnerKaton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerKaton : MonoBehaviour
+{
+    public GameObject katonPrefab;
+    float timeBeforeSpawn = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeBeforeSpawn -= Time.deltaTime;
+
+        if(timeBeforeSpawn < 0f)
+        {
+            GameObject go = Instantiate(katonPrefab, transform.position, Quaternion.identity);
+            go.transform.parent = gameObject.transform;
+            timeBeforeSpawn = 1f;
+        }
+    }
+}
